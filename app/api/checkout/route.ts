d8a3788445f01757bac8a87a258a4d511a5be717
Robert Norby <rhockey30@yahoo.com>
2026-04-09 19:51:17 -0600

    fix stripe api version

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -1,9 +1,8 @@
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2024-06-20",
-});
+import Stripe from "stripe";
 
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 export async function POST(req: Request) {
   const { location } = await req.json();
 
